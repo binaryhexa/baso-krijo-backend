f319b988f23ceaec9302c04d39fda38468cec406
binaryhexa <gif.glses@gmail.com>
2025-01-24 22:42:53 +0700

    feat: manajemen stok api

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 const menuRoutes = require("./routes/menuRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
+const stokRoutes = require('./routes/stokRoutes');
 
 const app = express();
 const PORT = 5000;
@@ -18,6 +19,7 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 app.use("/api/menu", menuRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/stok", stokRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
