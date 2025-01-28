d2faeb3c162d60cba796360d7595c4fdefa634ae
binaryhexa <gif.glses@gmail.com>
2025-01-29 01:20:53 +0700

    feat: api deployment (test)

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// 1. Modifikasi file server.js
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -10,7 +11,8 @@ const dashboardRoutes = require("./routes/dashboardRoutes");
 const stokRoutes = require('./routes/stokRoutes');
 
 const app = express();
-const PORT = 5000;
+// Menggunakan PORT dari environment variable
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -21,6 +23,11 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/stok", stokRoutes);
 
+// Menambahkan route default
+app.get('/', (req, res) => {
+  res.json({ message: 'API is running' });
+});
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
