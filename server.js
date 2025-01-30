28d7cd9aa073609065eee3e2b5c31789b69b8546
binaryhexa <gif.glses@gmail.com>
2025-01-30 23:19:56 +0700

    fix: db connection

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const stokRoutes = require('./routes/stokRoutes');
 const bahanBakuRoutes = require('./routes/bahanBakuRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
