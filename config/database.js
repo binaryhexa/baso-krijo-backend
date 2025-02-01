14802267735a4744e1a89b3d55d1e12aab38fb95
binaryhexa <gif.glses@gmail.com>
2025-02-01 14:55:08 +0700

    feat: deploy api

--- config/database.js
+++ config/database.js
@@ -1,19 +1,13 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  port: 3306
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error("Koneksi ke database gagal:", err);
-  } else {
-    console.log("Koneksi ke database berhasil.");
-  }
+const db = mysql.createPool({
+  host: process.env.MYSQL_ADDON_HOST,
+  database: process.env.MYSQL_ADDON_DB,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  port: process.env.MYSQL_ADDON_PORT,
+  ssl: true
 });
 
 module.exports = db;
